37d9a3194a3638b7cc7bb907f592a88eaa950e03
livia <liviamajzoubsilva19@gmail.com>
2025-04-17 16:50:50 -0300

    Update exercicio de repetição 2  -  parte 1.c

--- exercicio de repetição 2  -  parte 1.c
+++ exercicio de repetição 2  -  parte 1.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
 
-int main() {
-    float numero = 0, maior_numero;
-    int primeiro_numero = 1;
+int main()
+{
+    float numero, maior_numero;
 
-    while (numero >= 0) {
+    printf("Digite um numero: ");
+    scanf("%f", &numero);
+
+
+    if (numero < 0)
+    {
+        printf("Nenhum numero valido foi digitado.\n");
+        return 0;
+    }
+
+    maior_numero = numero;
+
+    while (1)
+    {
         printf("Digite um numero: ");
         scanf("%f", &numero);
 
-        if (numero < 0) {
+        if (numero < 0)
+        {
             break;
         }
 
-        if (primeiro_numero) {
-            maior_numero = numero;
-            primeiro_numero = 0;
-        } else if (numero > maior_numero) {
+        if (numero > maior_numero)
+        {
             maior_numero = numero;
         }
     }
 
-    if (!primeiro_numero) {
-        printf("O maior numero digitado foi: %.2f\n", maior_numero);
-    } else {
-        printf("Nenhum numero valido foi digitado.\n");
-    }
+    printf("O maior numero digitado foi: %.2f\n", maior_numero);
 
     return 0;
 }
